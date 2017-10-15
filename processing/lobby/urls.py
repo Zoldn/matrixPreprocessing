b7b9ddb095a670747117304d1ca9e501a1cc869b
Zoldn <mihail-svetkin@mail.ru>
2017-10-15 12:53:51 +0300

    LobbyListView с сортировками по полям

--- processing/lobby/urls.py
+++ processing/lobby/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^(?P<pk>\d+)/', views.LobbyShortView.as_view(), name='showLobby'),
-    url(r'^list/$', views.show_lobbies, name='showLobbies'),
+    url(r'^list/$', views.LobbyListView.as_view(), name='showLobbies'),
+    url(r'^create/$', views.create_lobby, name='createLobby'),
+    url(r'^join/(?P<lobby_id>\d+)/', views.join_lobby, name='joinLobby'),
 ]
--- processing/lobby/views.py
+++ processing/lobby/views.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 from .models import Lobby
 # Create your views here.
 
@@ -14,8 +14,33 @@ class LobbyShortView(DetailView):  # One-line view of Lobby for list
     template_name = 'lobby_template.html'
 
 
+class LobbyListView(ListView):
+
+    template_name = 'lobby_list.html'
+    model = Lobby
+
+    def dispatch(self, request, *args, **kwargs):
+        self.sort_field = request.GET.get('sort_field')
+        return super(LobbyListView, self).dispatch(request, *args, **kwargs)
+
+    def get_queryset(self):
+        queryset = Lobby.objects.all()
+        if self.sort_field:
+            queryset = queryset.order_by(self.sort_field)
+        return queryset[:10]
+
+
 def show_lobbies(request):
     lobbies = Lobby.objects.all()
     print lobbies[0]
     ret = render(request, 'lobby_list.html', {'lobbies': lobbies})
     return ret
+
+
+def create_lobby(request):
+    return HttpResponse('Lobby create')
+
+
+def join_lobby(request):
+    return HttpResponse('Join lobby')
+
